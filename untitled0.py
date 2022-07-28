ff2bf1122a99137bf24028a65c8edb5fbec0dee4
rakinplaban <rakinshahriar54@gmail.com>
2022-07-29 01:14:10 +0600

    Done!

--- untitled0.py
+++ untitled0.py
@@ -9,36 +9,14 @@ from collections import Counter
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # s1 = Counter(s)
-        # s2 = Counter(t)
-        list1,list2 = [],[]
-        
-        for i in s:
-            list1.append(i)
-            
-        list1.sort()
-            
-        for i in t:
-            list2.append(i)
-        list2.sort()
-        
-        flag = 0
-            
-        if len(list1) == len(list2) and len(s) == len(t):
-            for i in range(len(list1)):
-                #for j in range(len(list2)):
-                if list1[i] not in list2:
-                    flag = 1
-                    
-            if flag == 0 :
-                return True
-                
-        else:
-            return False
+        list1 = sorted(s)
+        list2 = sorted(t)
+        return list1 == list2
         
         
-list_a = "anagrama"
-list_b = "nagaramm"
+        
+list_a = "an"
+list_b = "na"
 t = Solution()
 t = t.isAnagram(list_a,list_b)
 print("List 1 = ",list_a)
